5aeca7be71c2c9d5ee7e63b71ee732de1142e8e8
Alex Low <me@alexlow.dev>
2021-04-24 23:26:20 -0400

    whee

--- netlify/functions/printful-webhooks/index.js
+++ netlify/functions/printful-webhooks/index.js
@@ -5,19 +5,21 @@ const sgMail = require("@sendgrid/mail")
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 exports.handler = async ({ body }) => {
-  console.log(body)
-  let type
-  let data
+  // console.log(body)
+  // let type
+  // let data
 
-  try {
-    ;({ type, data } = JSON.parse(body)) //re-descture type and data from body (adds that bonkers semicolon)
-  } catch (err) {
-    console.error(err)
-    return {
-      statusCode: 500,
-      body: "oops",
-    }
-  }
+  // try {
+  //   ;({ type, data } = JSON.parse(body)) //re-descture type and data from body (adds that bonkers semicolon)
+  // } catch (err) {
+  //   console.error(err)
+  //   return {
+  //     statusCode: 500,
+  //     body: "oops",
+  //   }
+  // }
+
+  const { type, data } = JSON.parse(body)
   const templates = {
     order_created: "d-7240134797ab443c898a0529d685ee73",
   }
