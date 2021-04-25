ee652990b5d0d875292b3a76d6131102461fad81
Alex Low <me@alexlow.dev>
2021-04-24 22:51:08 -0400

    test

--- netlify/functions/printful-webhooks/index.js
+++ netlify/functions/printful-webhooks/index.js
@@ -5,191 +5,42 @@ const sgMail = require("@sendgrid/mail")
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 exports.handler = async ({ body }) => {
-  // if (body.type === "order_created") {
-  const msg = {
-    to: "alexanderjameslow@gmail.com",
-    from: process.env.FROM_EMAIL_ADDRESS,
-    html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-      <html data-editor-version="2" class="sg-campaigns" xmlns="http://www.w3.org/1999/xhtml">
-          <head>
-            <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1">
-            <!--[if !mso]><!-->
-            <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-            <!--<![endif]-->
-            <!--[if (gte mso 9)|(IE)]>
-            <xml>
-              <o:OfficeDocumentSettings>
-                <o:AllowPNG/>
-                <o:PixelsPerInch>96</o:PixelsPerInch>
-              </o:OfficeDocumentSettings>
-            </xml>
-            <![endif]-->
-            <!--[if (gte mso 9)|(IE)]>
-        <style type="text/css">
-          body {width: 600px;margin: 0 auto;}
-          table {border-collapse: collapse;}
-          table, td {mso-table-lspace: 0pt;mso-table-rspace: 0pt;}
-          img {-ms-interpolation-mode: bicubic;}
-        </style>
-      <![endif]-->
-            <style type="text/css">
-          body, p, div {
-            font-family: arial,helvetica,sans-serif;
-            font-size: 14px;
-          }
-          body {
-            color: #ffffff;
-          }
-          body a {
-            color: #ffffff;
-            text-decoration: none;
-          }
-          p { margin: 0; padding: 0; }
-          table.wrapper {
-            width:100% !important;
-            table-layout: fixed;
-            -webkit-font-smoothing: antialiased;
-            -webkit-text-size-adjust: 100%;
-            -moz-text-size-adjust: 100%;
-            -ms-text-size-adjust: 100%;
-          }
-          img.max-width {
-            max-width: 100% !important;
-          }
-          .column.of-2 {
-            width: 50%;
-          }
-          .column.of-3 {
-            width: 33.333%;
-          }
-          .column.of-4 {
-            width: 25%;
-          }
-          ul ul ul ul  {
-            list-style-type: disc !important;
-          }
-          ol ol {
-            list-style-type: lower-roman !important;
-          }
-          ol ol ol {
-            list-style-type: lower-latin !important;
-          }
-          ol ol ol ol {
-            list-style-type: decimal !important;
-          }
-          @media screen and (max-width:480px) {
-            .preheader .rightColumnContent,
-            .footer .rightColumnContent {
-              text-align: left !important;
-            }
-            .preheader .rightColumnContent div,
-            .preheader .rightColumnContent span,
-            .footer .rightColumnContent div,
-            .footer .rightColumnContent span {
-              text-align: left !important;
-            }
-            .preheader .rightColumnContent,
-            .preheader .leftColumnContent {
-              font-size: 80% !important;
-              padding: 5px 0;
-            }
-            table.wrapper-mobile {
-              width: 100% !important;
-              table-layout: fixed;
-            }
-            img.max-width {
-              height: auto !important;
-              max-width: 100% !important;
-            }
-            a.bulletproof-button {
-              display: block !important;
-              width: auto !important;
-              font-size: 80%;
-              padding-left: 0 !important;
-              padding-right: 0 !important;
-            }
-            .columns {
-              width: 100% !important;
-            }
-            .column {
-              display: block !important;
-              width: 100% !important;
-              padding-left: 0 !important;
-              padding-right: 0 !important;
-              margin-left: 0 !important;
-              margin-right: 0 !important;
-            }
-            .social-icon-column {
-              display: inline-block !important;
-            }
-          }
-        </style>
-            <!--user entered Head Start--><!--End Head user entered-->
-          </head>
-          <body>
-            <center class="wrapper" data-link-color="#ffffff" data-body-style="font-size:14px; font-family:arial,helvetica,sans-serif; color:#ffffff; background-color:#000000;">
-              <div class="webkit">
-                <table cellpadding="0" cellspacing="0" border="0" width="100%" class="wrapper" bgcolor="#000000">
-                  <tr>
-                    <td valign="top" bgcolor="#000000" width="100%">
-                      <table width="100%" role="content-container" class="outer" align="center" cellpadding="0" cellspacing="0" border="0">
-                        <tr>
-                          <td width="100%">
-                            <table width="100%" cellpadding="0" cellspacing="0" border="0">
-                              <tr>
-                                <td>
-                                  <!--[if mso]>
-          <center>
-          <table><tr><td width="600">
-        <![endif]-->
-                                          <table width="100%" cellpadding="0" cellspacing="0" border="0" style="width:100%; max-width:600px;" align="center">
-                                            <tr>
-                                              <td role="modules-container" style="padding:0px 0px 0px 0px; color:#ffffff; text-align:left;" bgcolor="#000000" width="100%" align="left"><table class="module preheader preheader-hide" role="module" data-type="preheader" border="0" cellpadding="0" cellspacing="0" width="100%" style="display: none !important; mso-hide: all; visibility: hidden; opacity: 0; color: transparent; height: 0; width: 0;">
-          <tr>
-            <td role="module-content">
-              <p></p>
-            </td>
-          </tr>
-        </table><table class="module" role="module" data-type="text" border="0" cellpadding="0" cellspacing="0" width="100%" style="table-layout: fixed;" data-muid="6ffe928e-c319-4781-8f48-24751a1f6dcd">
-          <tbody>
-            <tr>
-              <td style="padding:18px 0px 18px 0px; line-height:40px; text-align:inherit;" height="100%" valign="top" bgcolor="" role="module-content"><div><h1 style="text-align: center"><span style="font-size: 48px; color: #ffffff">WHATS YOUR AVERAGE?</span></h1><div></div></div></td>
-            </tr>
-          </tbody>
-        </table><div data-role="module-unsubscribe" class="module" role="module" data-type="unsubscribe" style="color:#444444; font-size:12px; line-height:20px; padding:16px 16px 16px 16px; text-align:Center;" data-muid="4e838cf3-9892-4a6d-94d6-170e474d21e5"><div class="Unsubscribe--addressLine"></div><p style="font-size:12px; line-height:20px;"><a target="_blank" class="Unsubscribe--unsubscribeLink zzzzzzz" href="{{{unsubscribe}}}" style="">Unsubscribe</a> - <a href="{{{unsubscribe_preferences}}}" target="_blank" class="Unsubscribe--unsubscribePreferences" style="">Unsubscribe Preferences</a></p></div></td>
-                                            </tr>
-                                          </table>
-                                          <!--[if mso]>
-                                        </td>
-                                      </tr>
-                                    </table>
-                                  </center>
-                                  <![endif]-->
-                                </td>
-                              </tr>
-                            </table>
-                          </td>
-                        </tr>
-                      </table>
-                    </td>
-                  </tr>
-                </table>
-              </div>
-            </center>
-          </body>
-        </html>`,
+  const { type, data } = body
+  const templates = {
+    order_created: "d-7240134797ab443c898a0529d685ee73",
   }
 
-  sgMail
-    .send(msg)
-    .then(res => console.log(res[0].statusCode + "\n" + res[0].headers))
-    .catch(err => console.log(err))
-  // }
-  // else if (body.type === "order_canceled") {
-  // } else if (body.type === "order_failed") {
-  // } else if (body.type === "package_shipped") {
-  // } else if (body.type === "package_returned") {
+  if (type === "order_created") {
+    const {
+      shipping,
+      created,
+      updated,
+      recipient,
+      items,
+      incomeplete_items,
+      costs,
+      retail_costs,
+      pricing_breakdown,
+      shipments,
+      gift,
+      packing_slip,
+    } = data.order
+
+    const msg = {
+      to: "alexanderjameslow@gmail.com",
+      // to: recipient.email,
+      from: process.env.FROM_EMAIL_ADDRESS,
+      templateId: templates[type],
+      // dynamicTemplateData: {
+      //   name: recipient.name,
+      // },
+    }
+    sgMail.send({})
+  }
+  // else if (type === "order_canceled") {
+  // } else if (type === "order_failed") {
+  // } else if (type === "package_shipped") {
+  // } else if (type === "package_returned") {
   // }
 
   return {
