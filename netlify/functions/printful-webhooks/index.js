8adf77340728048dbad7e0dcb066584ed0e52924
Alex Low <me@alexlow.dev>
2021-04-26 11:33:03 -0400

    test auto email output

--- netlify/functions/printful-webhooks/index.js
+++ netlify/functions/printful-webhooks/index.js
@@ -8,8 +8,8 @@ exports.handler = async ({ body }) => {
   let type
   let data
 
-  // const test = JSON.parse(body)
-  // console.log(JSON.stringify(test, null, 2))
+  const test = JSON.parse(body)
+  console.log(JSON.stringify(test, null, 2))
 
   try {
     ;({ type, data } = JSON.parse(body)) //re-descture type and data from body (adds that bonkers semicolon)
